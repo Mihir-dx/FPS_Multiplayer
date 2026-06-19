ffebb7f95066aa392d43e59e8162b076f0d2e800
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-19 23:28:05 +0530

    Dropdown.h Initialize

---
+++ Source/DedicatedServers/Public/UI/Portal/Dropdown/Dropdown.h
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Dropdown.generated.h"
+
+
+class UImage;
+class UTextBlock;
+class UButton;
+class UWidgetSwitcher;
+
+UCLASS()
+class DEDICATEDSERVERS_API UDropdown : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWidgetSwitcher>	WidgetSwitcher;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget>	ExpandedWidget;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget>	CollapsedWidget;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Expander;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_ButtonText;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSlateColor HoveredTextColor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSlateColor UnHoveredTextColor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSlateBrush Triangle_Up;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSlateBrush Triangle_Down;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Image_Triangle;
+	
+protected:
+	
+	virtual void NativeConstruct() override;
+	void SetStyleTransparent();
+	virtual void NativePreConstruct() override;
+	
+	UFUNCTION()
+	void ToggleDropdown();
+	
+	bool bIsExpanded;
+	
+	void Expand();
+	void Collapse();
+	
+	UFUNCTION()
+	void Hover();
+	
+	UFUNCTION()
+	void UnHover();
+};
