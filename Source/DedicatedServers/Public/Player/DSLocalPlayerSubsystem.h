4ca0204e2e26f7393afae4697972848963e8553f
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-23 20:21:18 +0530

    Added AuthResut Function in .h

--- Source/DedicatedServers/Public/Player/DSLocalPlayerSubsystem.h
+++ Source/DedicatedServers/Public/Player/DSLocalPlayerSubsystem.h
@@ -19,6 +19,7 @@ public:
 	void InitializeToken(const FDSAuthenticationResult& AuthResult, TScriptInterface<IPortalManagement> PortalManagement);
 	void SetRefreshTokenTimer();
 	void UpdateTokens(const FString& AccessToken, const FString& IdToken);
+	FDSAuthenticationResult GetAuthResult() const;
 	
 private:
 	
