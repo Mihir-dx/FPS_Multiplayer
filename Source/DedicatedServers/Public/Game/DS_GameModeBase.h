bb196ae898cff165fb25df3ab5f12e3de0cf93a2
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-30 17:20:39 +0530

    Implemented Function declaration for Start, Stop and OnCountdown Timer in DS_GameModeBase.h

---
+++ Source/DedicatedServers/Public/Game/DS_GameModeBase.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Types/DSTypes.h"
+#include "DS_GameModeBase.generated.h"
+
+
+UCLASS()
+class DEDICATEDSERVERS_API ADS_GameModeBase : public AGameMode
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	void StartCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle);
+	void StopCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle);
+	virtual void OnCountdownTimerFinished(ECountdownTimerType Type);
+};
