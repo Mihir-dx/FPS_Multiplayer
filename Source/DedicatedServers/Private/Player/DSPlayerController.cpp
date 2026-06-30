8a1dcd83c41ae21046bfea573af3a61bbe3b6b1f
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-30 12:56:53 +0530

    Created and Implemented DSPlayerController.cpp, Dedicated PlayerController for each client to calculate time of Single Trip Time.

---
+++ Source/DedicatedServers/Private/Player/DSPlayerController.cpp
@@ -0,0 +1,27 @@
+﻿#include "Player/DSPlayerController.h"
+
+ADSPlayerController::ADSPlayerController()
+{
+	SingleTripTime = 0.f;
+}
+
+void ADSPlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+	if (GetNetMode() == NM_Standalone) return;
+	
+	if (IsLocalController())
+		Server_Ping(GetWorld()->GetTimeSeconds());
+}
+
+void ADSPlayerController::Server_Ping_Implementation(float TimeOfRequest)
+{
+	Client_Pong(TimeOfRequest);
+}
+
+void ADSPlayerController::Client_Pong_Implementation(float TimeOfRequest)
+{
+	const float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfRequest;
+	SingleTripTime = RoundTripTime * .5f;
+}
+
