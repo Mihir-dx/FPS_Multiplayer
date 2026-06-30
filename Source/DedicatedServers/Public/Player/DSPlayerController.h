bcce48179915ae2be829a4cbadcc7dbbfc13dfbb
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-30 12:56:38 +0530

    Created and Implemented DSPlayerController.h, Dedicated PlayerController for each client to calculate time of Single Trip Time.

---
+++ Source/DedicatedServers/Public/Player/DSPlayerController.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DSPlayerController.generated.h"
+
+
+UCLASS()
+class DEDICATEDSERVERS_API ADSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ADSPlayerController();
+	virtual void ReceivedPlayer() override;
+	
+protected:
+	UFUNCTION(Server, Reliable)
+	void Server_Ping(float TimeOfRequest);
+	
+	UFUNCTION(Client, Reliable)
+	void Client_Pong(float TimeOfRequest);
+	
+private:
+	float SingleTripTime;
+};
