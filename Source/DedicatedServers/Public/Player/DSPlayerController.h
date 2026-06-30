9f8f2b9c0d819aea6e6e96aaa522d4987cbdeff2
Mihir Kumar <mihirkumar1235@gmail.com>
2026-06-30 16:29:20 +0530

    Implemented Timer RPCs and Delegates in DSPlayerController.h

--- Source/DedicatedServers/Public/Player/DSPlayerController.h
+++ Source/DedicatedServers/Public/Player/DSPlayerController.h
@@ -1,9 +1,11 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "Types/DSTypes.h"
 #include "GameFramework/PlayerController.h"
 #include "DSPlayerController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTimerStateChangedDelegate, float, Time, ECountdownTimerType, Type);
 
 UCLASS()
 class DEDICATEDSERVERS_API ADSPlayerController : public APlayerController
@@ -14,11 +16,23 @@ public:
 	ADSPlayerController();
 	virtual void ReceivedPlayer() override;
 	
-protected:
-	UFUNCTION(Server, Reliable)
-	void Server_Ping(float TimeOfRequest);
+	UFUNCTION(Client, Reliable)
+	void Client_TimerUpdated(float CountdownTimeLeft, ECountdownTimerType Type) const;
 	
 	UFUNCTION(Client, Reliable)
+	void Client_TimerStopped(float CountdownTimeLeft, ECountdownTimerType Type) const;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnTimerStateChangedDelegate OnTimerUpdated;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnTimerStateChangedDelegate OnTimerStopped;
+	
+protected:
+	UFUNCTION(Server, Reliable)				//RPC
+	void Server_Ping(float TimeOfRequest);
+	
+	UFUNCTION(Client, Reliable)				//RPC
 	void Client_Pong(float TimeOfRequest);
 	
 private:
