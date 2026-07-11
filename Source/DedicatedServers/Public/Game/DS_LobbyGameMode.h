e86ef8b3650a32a8cca6813e4d29455b324fd7c5
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-11 20:13:06 +0530

    Declared definitions for MinPlayer in lobby and SeamlessTravelling in .h

--- Source/DedicatedServers/Public/Game/DS_LobbyGameMode.h
+++ Source/DedicatedServers/Public/Game/DS_LobbyGameMode.h
@@ -14,14 +14,35 @@ class DEDICATEDSERVERS_API ADS_LobbyGameMode : public ADS_GameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	ADS_LobbyGameMode();
+	void CheckAndStartLobbyCountdown();
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	
 protected:
 	
 	virtual void BeginPlay() override;
+	virtual void OnCountdownTimerFinished(ECountdownTimerType Type) override;
+	virtual void InitSeamlessTravelPlayer(AController* NewController) override;
+	void CheckAndStopLobbyCountdown();
+	virtual void Logout(AController* Exiting) override;
+	
+	UPROPERTY()
+	ELobbyStatus LobbyStatus;
+	
+	UPROPERTY(EditDefaultsOnly)
+	int32 MinPlayers;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> DestinationMap;	//Pointer To Map's Address
 	
 private:
 	
 	UPROPERTY()
 	TObjectPtr<UDS_GameInstanceSubsystem> DSGameInstanceSubsystem;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FCountdownTimerHandle LobbyCountdownTimer;
 	
 #if defined(WITH_GAMELIFT) && WITH_GAMELIFT
 	void SetServerParameters(FServerParameters& OutServerParameters);
