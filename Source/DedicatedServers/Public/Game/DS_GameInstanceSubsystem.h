25549ac0cf29215cb441bbd898e5b7b46b1bd80c
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-11 16:12:46 +0530

    Implemented Separate Instance of game by splitting DS_GameMode.h to DS_GameInstanceSubsystem.h which includes OnhealthCheck

---
+++ Source/DedicatedServers/Public/Game/DS_GameInstanceSubsystem.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "DS_GameInstanceSubsystem.generated.h"
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+#include "GameLiftServerSDK.h"
+#endif
+
+UCLASS()
+class DEDICATEDSERVERS_API UDS_GameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UDS_GameInstanceSubsystem();
+	
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+	void InitGameLift(const FServerParameters& ServerParams);
+#endif
+	
+	UPROPERTY(BlueprintReadOnly)
+	bool bGameLiftInitialized;
+	
+private:
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+	TSharedPtr<FProcessParameters> ProcessParameters;
+#endif
+	
+protected:
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+	void OnStartGameSession(Aws::GameLift::Server::Model::GameSession OnGameSession);
+	void OnProcessTerminate();
+	bool OnHealthCheck();
+#endif
+};
