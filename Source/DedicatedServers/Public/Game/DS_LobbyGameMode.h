2dc5d1cd92662530e301e7281fee18ecfbaf1df8
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-11 16:10:31 +0530

    Implemented Separate Lobby Gamemode splitting DS_GameMode.h to DS_LobbyGameMode.h

---
+++ Source/DedicatedServers/Public/Game/DS_LobbyGameMode.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DS_GameModeBase.h"
+#include "DS_LobbyGameMode.generated.h"
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+#include "GameLiftServerSDK.h"
+#endif
+
+class UDS_GameInstanceSubsystem;
+
+UCLASS()
+class DEDICATEDSERVERS_API ADS_LobbyGameMode : public ADS_GameModeBase
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY()
+	TObjectPtr<UDS_GameInstanceSubsystem> DSGameInstanceSubsystem;
+	
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+	void SetServerParameters(FServerParameters& OutServerParameters);
+#endif
+	
+	void InitGameLift();
+};
