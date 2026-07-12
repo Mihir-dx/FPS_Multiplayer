bc58885128a88134ed72fa47972699dd57ed13b4
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-12 21:19:17 +0530

    Added Soft ojb ptr to the Lobby Map in .h

---
+++ Source/DedicatedServers/Public/Game/DS_MatchGameMode.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DS_GameModeBase.h"
+#include "DS_MatchGameMode.generated.h"
+
+
+UCLASS()
+class DEDICATEDSERVERS_API ADS_MatchGameMode : public ADS_GameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+	ADS_MatchGameMode();
+	
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	
+	UPROPERTY()
+	EMatchStatus MatchStatus;
+	
+protected:
+	
+	virtual void OnCountdownTimerFinished(ECountdownTimerType Type) override;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FCountdownTimerHandle PreMatchTimer;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FCountdownTimerHandle MatchTimer;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FCountdownTimerHandle PostMatchTimer;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> LobbyMap;
+};
