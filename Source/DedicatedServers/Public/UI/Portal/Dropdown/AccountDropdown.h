7eb79c324a0c28894740fb009800d04fd4c09098
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-13 23:00:46 +0530

    Implemented AccountDropdown.h

---
+++ Source/DedicatedServers/Public/UI/Portal/Dropdown/AccountDropdown.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Dropdown.h"
+#include "AccountDropdown.generated.h"
+
+
+class UDSLocalPlayerSubsystem;
+
+UCLASS()
+class DEDICATEDSERVERS_API UAccountDropdown : public UDropdown
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	virtual void NativeConstruct() override;
+	
+private:
+	
+	UDSLocalPlayerSubsystem* GetLocalPlayerSubsystem() const;
+};
