d49004bc33c249985d2920b3d0ec8f1a9a6d02d4
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-16 20:10:14 +0530

    Implemented Function for Remove Player Session for Match in .cpp

--- Source/DedicatedServers/Private/Game/DS_MatchGameMode.cpp
+++ Source/DedicatedServers/Private/Game/DS_MatchGameMode.cpp
@@ -1,5 +1,7 @@
 ﻿#include "Game/DS_MatchGameMode.h"
 
+#include "Player/DSPlayerController.h"
+
 ADS_MatchGameMode::ADS_MatchGameMode()
 {
 	bUseSeamlessTravel = true;
@@ -20,6 +22,12 @@ void ADS_MatchGameMode::PostLogin(APlayerController* NewPlayer)
 	}
 }
 
+void ADS_MatchGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+	RemovePlayerSession(Exiting);
+}
+
 void ADS_MatchGameMode::OnCountdownTimerFinished(ECountdownTimerType Type)
 {
 	Super::OnCountdownTimerFinished(Type);
@@ -28,15 +36,29 @@ void ADS_MatchGameMode::OnCountdownTimerFinished(ECountdownTimerType Type)
 	{
 		MatchStatus = EMatchStatus::Match;
 		StartCountdownTimer(MatchTimer);
+		SetClientInputEnabled(true);
 	}
 	if (Type == ECountdownTimerType::Match)
 	{
 		MatchStatus = EMatchStatus::PostMatch;
 		StartCountdownTimer(PostMatchTimer);
+		SetClientInputEnabled(false);
 	}
 	if (Type == ECountdownTimerType::PostMatch)
 	{
 		MatchStatus = EMatchStatus::SeamlessTravelling;
 		TrySeamlessTravel(LobbyMap);
+	}
+}
+
+void ADS_MatchGameMode::SetClientInputEnabled(bool bEnabled)
+{
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Iterator->Get());
+		if (IsValid(DSPlayerController))
+		{
+			DSPlayerController->Client_SetInputEnabled(bEnabled);
+		}
 	}
 }
