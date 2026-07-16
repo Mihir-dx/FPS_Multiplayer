dca98ed5b56d52635e4b3f2bf1fdc2df226f3eb8
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-16 20:11:18 +0530

    Created a simplified function to remove player (exiting the game) in .cpp

--- Source/DedicatedServers/Private/Game/DS_GameModeBase.cpp
+++ Source/DedicatedServers/Private/Game/DS_GameModeBase.cpp
@@ -1,4 +1,6 @@
 ﻿#include "Game/DS_GameModeBase.h"
+#include "GameLiftServerSDK.h"
+#include "Kismet/GameplayStatics.h"
 #include "Player/DSPlayerController.h"
 
 void ADS_GameModeBase::StartCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
@@ -17,15 +19,7 @@ void ADS_GameModeBase::StartCountdownTimer(FCountdownTimerHandle& CountdownTimer
 	
 	CountdownTimerHandle.TimerUpdateDelegate.BindWeakLambda(this, [&]()
 	{
-		for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
-		{
-			ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Iterator->Get());
-			if (IsValid(DSPlayerController))
-			{
-				const float CountdownTimeLeft = CountdownTimerHandle.CountdownTime - GetWorldTimerManager().GetTimerElapsed(CountdownTimerHandle.TimerFinishedHandle);
-				DSPlayerController->Client_TimerUpdated(CountdownTimeLeft, CountdownTimerHandle.Type);
-			}
-		}
+		UpdateCountdownTimer(CountdownTimerHandle);
 	});
 	
 	GetWorldTimerManager().SetTimer(
@@ -33,6 +27,7 @@ void ADS_GameModeBase::StartCountdownTimer(FCountdownTimerHandle& CountdownTimer
 		CountdownTimerHandle.TimerUpdateDelegate,
 		CountdownTimerHandle.CountdownUpdateInterval,
 		true);
+	UpdateCountdownTimer(CountdownTimerHandle);
 }
 
 void ADS_GameModeBase::StopCountdownTimer(FCountdownTimerHandle& CountdownTimerHandle)
@@ -58,7 +53,50 @@ void ADS_GameModeBase::StopCountdownTimer(FCountdownTimerHandle& CountdownTimerH
 	}
 }
 
+void ADS_GameModeBase::UpdateCountdownTimer(const FCountdownTimerHandle& CountdownTimerHandle)
+{
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Iterator->Get());
+		if (IsValid(DSPlayerController))
+		{
+			const float CountdownTimeLeft = CountdownTimerHandle.CountdownTime - GetWorldTimerManager().GetTimerElapsed(CountdownTimerHandle.TimerFinishedHandle);
+			DSPlayerController->Client_TimerUpdated(CountdownTimeLeft, CountdownTimerHandle.Type);
+		}
+	}
+}
+
 void ADS_GameModeBase::OnCountdownTimerFinished(ECountdownTimerType Type)
 {
 	
+}
+
+void ADS_GameModeBase::TrySeamlessTravel(TSoftObjectPtr<UWorld> DestinationMap)
+{
+	//Server Travel
+	const FString MapName = DestinationMap.ToSoftObjectPath().GetAssetName();
+	if (GIsEditor)
+	{
+		//To Play in Standalone Mode (In Editor)
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, DestinationMap);
+	}
+	else
+	{
+		//To play in any server -> Gamelift EC2 or Anywhere
+		GetWorld()->ServerTravel(MapName);
+	}
+}
+
+void ADS_GameModeBase::RemovePlayerSession(AController* Exiting)
+{
+	ADSPlayerController* DSPlayerController = Cast<ADSPlayerController>(Exiting);
+	if (!IsValid(DSPlayerController)) return;
+	
+#if defined(WITH_GAMELIFT) && WITH_GAMELIFT
+	
+	const FString& PlayerSessionId = DSPlayerController->PlayerSessionId;
+	if (!PlayerSessionId.IsEmpty())
+		Aws::GameLift::Server::RemovePlayerSession(TCHAR_TO_ANSI(*PlayerSessionId));
+
+#endif
 }
