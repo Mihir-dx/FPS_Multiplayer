8a4c00400e9369ee0d3f715313bf37de1dbd6541
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-17 18:38:30 +0530

    Overridden InitSeamlessTravelPlayer so the timer could load in .h

--- Source/DedicatedServers/Public/Game/DS_MatchGameMode.h
+++ Source/DedicatedServers/Public/Game/DS_MatchGameMode.h
@@ -16,6 +16,7 @@ public:
 	
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	virtual void Logout(AController* Exiting) override;
+	virtual void InitSeamlessTravelPlayer(AController* NewController) override;
 	
 	UPROPERTY()
 	EMatchStatus MatchStatus;
