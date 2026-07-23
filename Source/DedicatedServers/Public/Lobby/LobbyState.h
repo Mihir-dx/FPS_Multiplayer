dec58165177f83573f5b599d119d66175a65a9bd
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-23 15:59:40 +0530

    Implemented function declaration Lobby State for player addition and removal in .h

---
+++ Source/DedicatedServers/Public/Lobby/LobbyState.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "LobbyPlayerInfo.h"
+#include "GameFramework/Info.h"
+#include "LobbyState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerInfoChanged, const FLobbyPlayerInfo&, PlayerInfo);
+
+UCLASS()
+class DEDICATEDSERVERS_API ALobbyState : public AInfo
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ALobbyState();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerInfoChanged OnPlayerInfoAdded;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerInfoChanged OnPlayerInfoRemoved;
+	
+	void AddPlayerInfo(const FLobbyPlayerInfo& PlayerInfo);
+	void RemovePlayerInfo(const FString& Username);
+	
+protected:
+	
+	UFUNCTION()
+	void OnRep_LobbyPlayerInfo();
+	
+private:
+		
+	UPROPERTY(ReplicatedUsing=OnRep_LobbyPlayerInfo)
+	FLobbyPlayerInfoArray PlayerInfoArray;
+};
