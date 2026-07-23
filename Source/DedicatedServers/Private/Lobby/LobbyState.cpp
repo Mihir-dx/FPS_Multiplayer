f907efb789272548bb7d5b196757e53da74a6fd5
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-23 16:00:02 +0530

    Implemented function definition in LobbyState for player addition and removal in .cpp

---
+++ Source/DedicatedServers/Private/Lobby/LobbyState.cpp
@@ -0,0 +1,30 @@
+﻿#include "Lobby/LobbyState.h"
+#include "Net/UnrealNetwork.h"
+
+ALobbyState::ALobbyState()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bAlwaysRelevant = true;
+}
+
+void ALobbyState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ALobbyState, PlayerInfoArray);	// Marked for Replication
+}
+
+void ALobbyState::AddPlayerInfo(const FLobbyPlayerInfo& PlayerInfo)
+{
+	PlayerInfoArray.AddPlayer(PlayerInfo);
+}
+
+void ALobbyState::RemovePlayerInfo(const FString& Username)
+{
+	PlayerInfoArray.RemovePlayer(Username);
+}
+
+void ALobbyState::OnRep_LobbyPlayerInfo()
+{
+	
+}
