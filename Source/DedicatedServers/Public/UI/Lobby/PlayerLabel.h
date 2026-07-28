1db8f4c121059103c0973f2b4a99c649e7caa3fb
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-28 16:50:59 +0530

    Player Label

---
+++ Source/DedicatedServers/Public/UI/Lobby/PlayerLabel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "PlayerLabel.generated.h"
+
+
+UCLASS()
+class DEDICATEDSERVERS_API UPlayerLabel : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetUsername(const FString& Username) const;
+	FString GetUsername() const;
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Username;
+};
