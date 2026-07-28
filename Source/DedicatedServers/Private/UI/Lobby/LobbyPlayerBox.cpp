1656c41a5e7033435235809fb110c4e5994bc687
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-28 17:32:44 +0530

    Created function declaration and definition of ScrollBox in game to see player info.

---
+++ Source/DedicatedServers/Private/UI/Lobby/LobbyPlayerBox.cpp
@@ -0,0 +1,71 @@
+﻿#include "UI/Lobby/LobbyPlayerBox.h"
+#include "Components/ScrollBox.h"
+#include "Game/DS_GameState.h"
+#include "Lobby/LobbyState.h"
+#include "UI/Lobby/PlayerLabel.h"
+
+void ULobbyPlayerBox::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	ADS_GameState* DSGameState = GetWorld()->GetGameState<ADS_GameState>();
+	if (!IsValid(DSGameState)) return;
+	
+	if (IsValid(DSGameState->LobbyState))
+	{
+		OnLobbyStateInitialized(DSGameState->LobbyState);
+	}
+	else
+	{
+		DSGameState->OnLobbyStateInitialized.AddDynamic(this, &ULobbyPlayerBox::OnLobbyStateInitialized);
+	}
+}
+
+void ULobbyPlayerBox::UpdatePlayerInfo(ALobbyState* LobbyState)
+{
+	ScrollBox_PlayerInfo->ClearChildren();
+	for (const FLobbyPlayerInfo& PlayerInfo : LobbyState->GetPlayers())
+	{
+		CreateAndAddPlayerLabel(PlayerInfo);
+	}
+}
+
+void ULobbyPlayerBox::OnLobbyStateInitialized(ALobbyState* LobbyState)
+{
+	 if (!IsValid(LobbyState)) return;
+	
+	LobbyState->OnPlayerInfoAdded.AddDynamic(this, &ULobbyPlayerBox::CreateAndAddPlayerLabel);
+	LobbyState->OnPlayerInfoRemoved.AddDynamic(this, &ULobbyPlayerBox::OnPlayerRemoved);
+}
+
+void ULobbyPlayerBox::CreateAndAddPlayerLabel(const FLobbyPlayerInfo& PlayerInfo)
+{
+	if (FindPlayerLabel(PlayerInfo.Username)) return;
+	
+	UPlayerLabel* PlayerLabel = CreateWidget<UPlayerLabel>(this, PlayerLabelClass);
+	if (!IsValid(PlayerLabel)) return;
+	
+	PlayerLabel->SetUsername(PlayerInfo.Username);
+	ScrollBox_PlayerInfo->AddChild(PlayerLabel);
+}
+
+void ULobbyPlayerBox::OnPlayerRemoved(const FLobbyPlayerInfo& PlayerInfo)
+{
+	if (UPlayerLabel* PlayerLabel = FindPlayerLabel(PlayerInfo.Username))
+	{
+		ScrollBox_PlayerInfo->RemoveChild(PlayerLabel);
+	}
+}
+
+UPlayerLabel* ULobbyPlayerBox::FindPlayerLabel(const FString& Username)
+{
+	for (UWidget* Child : ScrollBox_PlayerInfo->GetAllChildren())
+	{
+		UPlayerLabel* PlayerLabel = Cast<UPlayerLabel>(Child);
+		if (IsValid(PlayerLabel) && PlayerLabel->GetUsername() == Username)
+		{
+			return PlayerLabel;
+		}
+	}
+	return nullptr;
+}
