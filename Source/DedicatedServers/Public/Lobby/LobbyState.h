81cc11a20e240fa0fb7841dd0b45fbe72f51cff9
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-28 17:30:54 +0530

    Created the function GetPlayers to loop over private variable FLobbyPlayerInfoArray PlayerInfoArray

--- Source/DedicatedServers/Public/Lobby/LobbyState.h
+++ Source/DedicatedServers/Public/Lobby/LobbyState.h
@@ -38,6 +38,8 @@ public:
 	void AddPlayerInfo(const FLobbyPlayerInfo& PlayerInfo);
 	void RemovePlayerInfo(const FString& Username);
 	
+	TArray<FLobbyPlayerInfo> GetPlayers() const;
+	
 protected:
 	
 	UFUNCTION()
