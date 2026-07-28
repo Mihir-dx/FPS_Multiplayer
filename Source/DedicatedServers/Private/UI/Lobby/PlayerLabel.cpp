716b2982e26c5a552d57071be75e5d51f6119fc3
Mihir Kumar <mihirkumar1235@gmail.com>
2026-07-28 16:51:15 +0530

    PlayerLabel.cpp

---
+++ Source/DedicatedServers/Private/UI/Lobby/PlayerLabel.cpp
@@ -0,0 +1,11 @@
+﻿#include "UI/Lobby/PlayerLabel.h"
+
+void UPlayerLabel::SetUsername(const FString& Username) const
+{
+	TextBlock_Username->SetText(FText::FromString(Username));
+}
+
+FString UPlayerLabel::GetUsername() const
+{
+	return TextBlock_Username->GetText().ToString();
+}
