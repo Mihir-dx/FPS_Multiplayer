1d2403d46c591e1bf03775bb22915a5372ec30e4
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-07 16:22:12 +0530

    Implemented Match Stats namespace in .cpp

---
+++ Source/DedicatedServers/Private/GameplayTags/DedicatedServersTags.cpp
@@ -0,0 +1,24 @@
+#include "GameplayTags/DedicatedServersTags.h"
+
+namespace DedicatedServersTags
+{
+	namespace GameSessionsAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ListFleets, "DedicatedServersTags.GameSessionsAPI.ListFleets", "List Fleets resources on the GameSessions API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FindOrCreateGameSession, "DedicatedServersTags.GameSessionsAPI.FindOrCreateGameSession", "Retrieves an Active Game session, or Create one if no session exist on the GameSessions API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(CreatePlayerSession, "DedicatedServersTags.GameSessionsAPI.CreatePlayerSession", "Creates a new Player Session on the GameSessions API")
+	}
+	
+	namespace PortalAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignUp, "DedicatedServersTags.PortalAPI.SignUp", "Creates a new user in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignIn, "DedicatedServersTags.PortalAPI.SignIn", "Retrieves Token in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ConfirmSignUp, "DedicatedServersTags.PortalAPI.ConfirmSignUp", "Confirms new user in Portal API")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(SignOut, "DedicatedServersTags.PortalAPI.SignOut", "Confirms SignOut and invalidates Tokens in Portal API")
+	}
+	
+	namespace GameStatsAPI
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(RecordMatchStats, "DedicatedServersTags.GameStatsAPI.RecordMatchStats", "Record the stats of a match in GameStats API")
+	}
+}
