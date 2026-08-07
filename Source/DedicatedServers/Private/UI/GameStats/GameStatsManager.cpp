0d81beceff53916656184e3a8b1fbc1e427c8837
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-07 16:57:29 +0530

    Implemented function - RecordMatchStats and bind it to APIData

---
+++ Source/DedicatedServers/Private/UI/GameStats/GameStatsManager.cpp
@@ -0,0 +1,25 @@
+﻿#include "UI/GameStats/GameStatsManager.h"
+#include "HttpModule.h"
+#include "JsonObjectConverter.h"
+#include "Data/API/APIData.h"
+#include "Interfaces/IHttpRequest.h"
+#include "UI/HTTP/HTTPRequestTypes.h"
+
+void UGameStatsManager::RecordMatchStats(const FDSRecordMatchStatsInput& RecordMatchStatsInput)
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+	FJsonObjectConverter::UStructToJsonObject(FDSRecordMatchStatsInput::StaticStruct(), &RecordMatchStatsInput, JsonObject.ToSharedRef());
+	
+	FString JsonString;
+	FJsonObjectConverter::UStructToJsonObjectString(FDSRecordMatchStatsInput::StaticStruct(), &RecordMatchStatsInput, JsonString);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 600.f, FColor::Blue, JsonString);
+	
+	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	const FString ApiUrl = APIData->GetAPIEndpoint(DedicatedServersTags::GameStatsAPI::RecordMatchStats);
+	Request->SetURL(ApiUrl);
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetContentAsString(JsonString);
+	Request->ProcessRequest();
+}
