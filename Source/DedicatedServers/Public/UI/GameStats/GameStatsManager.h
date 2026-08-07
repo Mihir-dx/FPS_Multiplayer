3ec66e99d0bbaeade399cfcc7485061878148063
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-07 16:56:59 +0530

    Implemented function - RecordMatchStats

---
+++ Source/DedicatedServers/Public/UI/GameStats/GameStatsManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/HTTP/HTTPRequestManager.h"
+#include "GameStatsManager.generated.h"
+
+
+struct FDSRecordMatchStatsInput;
+
+UCLASS()
+class DEDICATEDSERVERS_API UGameStatsManager : public UHTTPRequestManager
+{
+	GENERATED_BODY()
+	
+private:
+	void RecordMatchStats(const FDSRecordMatchStatsInput& RecordMatchStatsInput);
+};
