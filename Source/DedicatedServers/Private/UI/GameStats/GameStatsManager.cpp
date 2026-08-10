e2a9c60883125ed2a2a01c40a8b2c3eac3bd4808
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-10 21:25:52 +0530

    Implemented RetrieveMatchStats function in .cpp

--- Source/DedicatedServers/Private/UI/GameStats/GameStatsManager.cpp
+++ Source/DedicatedServers/Private/UI/GameStats/GameStatsManager.cpp
@@ -5,6 +5,7 @@
 #include "DedicatedServers/DedicatedServers.h"
 #include "Interfaces/IHttpRequest.h"
 #include "Interfaces/IHttpResponse.h"
+#include "Player/DSLocalPlayerSubsystem.h"
 #include "UI/HTTP/HTTPRequestTypes.h"
 
 void UGameStatsManager::RecordMatchStats(const FDSRecordMatchStatsInput& RecordMatchStatsInput)
@@ -22,7 +23,7 @@ void UGameStatsManager::RecordMatchStats(const FDSRecordMatchStatsInput& RecordM
 }
 
 void UGameStatsManager::RecordMatchStats_Response(FHttpRequestPtr Request, FHttpResponsePtr Response,
-	bool bWasSuccessful)
+                                                  bool bWasSuccessful)
 {
 	if (!bWasSuccessful)
 	{
@@ -35,5 +36,54 @@ void UGameStatsManager::RecordMatchStats_Response(FHttpRequestPtr Request, FHttp
 	if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
 	{
 		ContainsErrors(JsonObject);
+	}
+}
+
+void UGameStatsManager::RetrieveMatchStats()
+{
+	UDSLocalPlayerSubsystem* LocalPlayerSubsystem = GetDSLocalPlayerSubsystem();
+	if (!IsValid(LocalPlayerSubsystem)) return;
+	check(APIData);
+	
+	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	const FString ApiUrl = APIData->GetAPIEndpoint(DedicatedServersTags::GameStatsAPI::RetrieveMatchStats);
+	Request->OnProcessRequestComplete().BindUObject(this, &UGameStatsManager::RetrieveMatchStats_Response);
+	Request->SetURL(ApiUrl);
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+	
+	TMap<FString, FString> Params = {
+		{TEXT("accessToken"), LocalPlayerSubsystem->GetAuthResult().AccessToken},
+	};
+	const FString Content = SerializeJsonContent(Params);
+	
+	Request->SetContentAsString(Content);
+	Request->ProcessRequest();
+}
+
+void UGameStatsManager::RetrieveMatchStats_Response(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	if (!bWasSuccessful)
+	{
+		OnRetrieveMatchStatsResponseReceived.Broadcast(FDSRetrieveMatchStatsResponse());
+		return;
+	}
+    
+	TSharedPtr<FJsonObject> JsonObject;
+	TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+    
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
+	{
+		if (ContainsErrors(JsonObject))
+		{
+			OnRetrieveMatchStatsResponseReceived.Broadcast(FDSRetrieveMatchStatsResponse());
+			return;
+		}
+        
+		FDSRetrieveMatchStatsResponse RetrieveMatchStatsResponse;
+		FJsonObjectConverter::JsonObjectToUStruct(JsonObject.ToSharedRef(), &RetrieveMatchStatsResponse);
+		RetrieveMatchStatsResponse.Dump();
+        
+		OnRetrieveMatchStatsResponseReceived.Broadcast(RetrieveMatchStatsResponse);
 	}
 }
