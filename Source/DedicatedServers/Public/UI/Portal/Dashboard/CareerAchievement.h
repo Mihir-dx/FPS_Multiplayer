b5b9c9f4a15a9e8da1fa6915f626da5c17137957
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-11 23:01:42 +0530

    Implemented function declaration for achievement name and its values in .h file

---
+++ Source/DedicatedServers/Public/UI/Portal/Dashboard/CareerAchievement.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CareerAchievement.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class DEDICATEDSERVERS_API UCareerAchievement : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetAchievementText(const FString& AchievementName, int32 AchievementValue) const;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_AchievementName;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_AchievementValue;
+};
