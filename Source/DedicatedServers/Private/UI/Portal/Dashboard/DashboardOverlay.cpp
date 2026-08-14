9d6f4347b65f21b76217e2a074f9d775c2fbcb5b
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-14 20:17:01 +0530

    Defined GameStatsManager and Disabled in .cpp

--- Source/DedicatedServers/Private/UI/Portal/Dashboard/DashboardOverlay.cpp
+++ Source/DedicatedServers/Private/UI/Portal/Dashboard/DashboardOverlay.cpp
@@ -1,9 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/Portal/Dashboard/DashboardOverlay.h"
 #include "Components/Button.h"
 #include "Components/WidgetSwitcher.h"
+#include "UI/GameStats/GameStatsManager.h"
 #include "UI/Portal/Dashboard/CareerPage.h"
 #include "UI/Portal/Dashboard/GamePage.h"
 #include "UI/Portal/Dashboard/LeaderboardPage.h"
@@ -11,6 +9,10 @@
 void UDashboardOverlay::NativeConstruct()
 {
 	Super::NativeConstruct();
+	
+	GameStatsManager = NewObject<UGameStatsManager>(this, GameStatsManagerClass);
+	GameStatsManager->OnRetrieveMatchStatsResponseReceived.AddDynamic(CareerPage, &UCareerPage::OnRetrieveMatchStats);
+	GameStatsManager->RetrieveMatchStatusMessage.AddDynamic(CareerPage, &UCareerPage::SetStatusMessage);
 	
 	GameButton->OnClicked.AddDynamic(this, &UDashboardOverlay::ShowGamePage);
 	CareerButton->OnClicked.AddDynamic(this, &UDashboardOverlay::ShowCareerPage);
@@ -21,15 +23,27 @@ void UDashboardOverlay::NativeConstruct()
 
 void UDashboardOverlay::ShowGamePage()
 {
+	DisableButton(GameButton);
 	WidgetSwitcher->SetActiveWidget(GamePage);
 }
 
 void UDashboardOverlay::ShowCareerPage()
 {
+	DisableButton(CareerButton);
 	WidgetSwitcher->SetActiveWidget(CareerPage);
+	GameStatsManager->RetrieveMatchStats();
 }
 
 void UDashboardOverlay::ShowLeaderboardPage()
 {
+	DisableButton(LeaderboardButton);
 	WidgetSwitcher->SetActiveWidget(LeaderboardPage);
+}
+
+void UDashboardOverlay::DisableButton(UButton* Button) const
+{
+	GameButton->SetIsEnabled(true);
+	CareerButton->SetIsEnabled(true);
+	LeaderboardButton->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
 }
