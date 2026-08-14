e4c809b0d90e3a5752744692bb0f055734a77ec0
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-14 21:07:27 +0530

    Added Wins and Losses in OnRetrieveMatchStats

--- Source/DedicatedServers/Private/UI/Portal/Dashboard/CareerPage.cpp
+++ Source/DedicatedServers/Private/UI/Portal/Dashboard/CareerPage.cpp
@@ -1,5 +1,6 @@
 #include "UI/Portal/Dashboard/CareerPage.h"
 #include "Components/TextBlock.h"
+#include "UI/HTTP/HTTPRequestTypes.h"
 
 void UCareerPage::NativeConstruct()
 {
@@ -12,7 +13,9 @@ void UCareerPage::NativeConstruct()
 
 void UCareerPage::OnRetrieveMatchStats(const FDSRetrieveMatchStatsResponse& RetrieveMatchStatsResponse)
 {
-	
+	TextBlock_Username->SetText(FText::FromString(RetrieveMatchStatsResponse.username));
+	TextBlock_Wins->SetText(FText::AsNumber(RetrieveMatchStatsResponse.matchWins));
+	TextBlock_Losses->SetText(FText::AsNumber(RetrieveMatchStatsResponse.matchLosses));
 }
 
 void UCareerPage::SetStatusMessage(const FString& StatusMessage, bool bShouldResetWidgets)
