46f67de11f86852bad91b938f474567567ef75ce
Mihir Kumar <mihirkumar1235@gmail.com>
2026-08-14 21:04:35 +0530

    Created ShooterCareerPage.h and added USpecialElimData and UCareerAchievement classes

---
+++ Source/FPSTemplate/Public/UI/Career/ShooterCareerPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Portal/Dashboard/CareerPage.h"
+#include "ShooterCareerPage.generated.h"
+
+class USpecialElimData;
+struct FDSRetrieveMatchStatsResponse;
+
+UCLASS()
+class FPSTEMPLATE_API UShooterCareerPage : public UCareerPage
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnRetrieveMatchStats(const FDSRetrieveMatchStatsResponse& RetrieveMatchStatsResponse) override;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USpecialElimData> SpecialElimDataClass;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UCareerAchievement> CareerAchievementClass;
+};
