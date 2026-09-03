d3acaf5405a1bcb64038d16d45c68303effee2a1
Mihir Kumar <mihirkumar1235@gmail.com>
2026-09-03 21:53:20 +0530

    Declared funtion - SetPlayerInfo and UI widgets in .h

---
+++ Source/DedicatedServers/Public/UI/Portal/Dashboard/LeaderboardCard.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LeaderboardCard.generated.h"
+
+
+class UTextBlock;
+
+UCLASS()
+class DEDICATEDSERVERS_API ULeaderboardCard : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	void SetPlayerInfo(const FString& Username, int32 Wins, int32 Place) const;
+	
+private:
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Username;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Wins;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Place;
+};
