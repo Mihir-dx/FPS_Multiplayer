9b30de4001320ecc565fd2e6bcc60cdf9a264bbd
Mihir Kumar <mihirkumar1235@gmail.com>
2026-09-17 19:29:22 +0530

    Defined function for AutoSignIn in .cpp

---
+++ Source/DedicatedServers/Private/UI/Portal/SignIn/SignInOverlay.cpp
@@ -0,0 +1,125 @@
+#include "UI/Portal/SignIn/SignInOverlay.h"
+#include "UI/Portal/PortalManager.h"
+#include "UI/API/GameSessions/JoinGame.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "UI/Portal/SignIn/ConfirmSignUpPage.h"
+#include "UI/Portal/SignIn/SignInPage.h"
+#include "UI/Portal/SignIn/SignUpPage.h"
+#include "UI/Portal/SignIn/SuccessConfirmedPage.h"
+#include "Components/EditableTextBox.h"
+#include "Components/TextBlock.h"
+#include "Player/DSLocalPlayerSubsystem.h"
+
+void USignInOverlay::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	check(PortalManagerClass);
+	
+	PortalManager = NewObject<UPortalManager>(this, PortalManagerClass);
+	
+	//SignInPage
+	SignInPage->Button_SignIn->OnClicked.AddDynamic(this, &USignInOverlay::SignInButtonClicked);
+	PortalManager->SignInStatusMessageDelegate.AddDynamic(SignInPage, &USignInPage::UpdateStatusMessage);
+	
+	//SignUpPage
+	SignInPage->Button_SignUp->OnClicked.AddDynamic(this, &USignInOverlay::ShowSignUpPage);
+	
+	//Quit Game
+	SignInPage->Button_Quit->OnClicked.AddDynamic(PortalManager, &UPortalManager::QuitGame);
+	
+	//Back Button Functionality
+	SignUpPage->Button_Back->OnClicked.AddDynamic(this, &USignInOverlay::ShowSignInPage);
+	
+	//SignUp Button Clicked
+	SignUpPage->Button_SignUp->OnClicked.AddDynamic(this, &USignInOverlay::SignUpButtonClicked);
+	PortalManager->SignUpStatusMessageDelegate.AddDynamic(SignUpPage, &USignUpPage::UpdateStatusMessage);
+	PortalManager->OnSignUpSucceeded.AddDynamic(this, &USignInOverlay::OnSignUpSucceeded);
+	
+	//Confirm SignUp
+	ConfirmSignUpPage->Button_Confirm->OnClicked.AddDynamic(this, &USignInOverlay::ConfirmButtonClicked);
+	ConfirmSignUpPage->Button_Back->OnClicked.AddDynamic(this, &USignInOverlay::ShowSignUpPage);
+	PortalManager->OnConfirmSucceeded.AddDynamic(this, &USignInOverlay::OnConfirmSucceeded);
+	PortalManager->ConfirmStatusMessageDelegate.AddDynamic(ConfirmSignUpPage, &UConfirmSignUpPage::UpdateStatusMessage);
+	
+	//Success Confirmed Page
+	SuccessConfirmedPage->Button_Ok->OnClicked.AddDynamic(this, &USignInOverlay::ShowSignInPage);
+	
+	//Automatic Signin
+	ShowSignInPage();
+	AutoSignIn();
+}
+
+void USignInOverlay::AutoSignIn()
+{
+	if (UDSLocalPlayerSubsystem* DSLocalPlayerSubsystem = PortalManager->GetDSLocalPlayerSubsystem(); IsValid(DSLocalPlayerSubsystem))
+	{
+		const FString& Username = DSLocalPlayerSubsystem->Username;
+		const FString& Password = DSLocalPlayerSubsystem->Password;
+		if (Username.IsEmpty() || Password.IsEmpty()) return;
+		
+		SignInPage->Button_SignIn->SetIsEnabled(false);
+		PortalManager->SignIn(Username, Password);
+	}
+}
+
+void USignInOverlay::ShowSignInPage()
+{
+	SignInPage->Button_SignIn->SetIsEnabled(true);
+	WidgetSwitcher->SetActiveWidget(SignInPage);
+}
+
+void USignInOverlay::ShowSignUpPage()
+{
+	WidgetSwitcher->SetActiveWidget(SignUpPage);
+}
+
+void USignInOverlay::ShowConfirmSignUpPage()
+{
+	WidgetSwitcher->SetActiveWidget(ConfirmSignUpPage);
+}
+
+void USignInOverlay::ShowSuccessConfirmedPage()
+{
+	WidgetSwitcher->SetActiveWidget(SuccessConfirmedPage);
+}
+
+void USignInOverlay::SignInButtonClicked()
+{
+	const FString Username = SignInPage->TextBox_UserName->GetText().ToString();
+	const FString Password = SignInPage->TextBox_Password->GetText().ToString();
+	if (UDSLocalPlayerSubsystem* DSLocalPlayerSubsystem = PortalManager->GetDSLocalPlayerSubsystem(); IsValid(DSLocalPlayerSubsystem))
+	{
+		DSLocalPlayerSubsystem->Password = Password;		//Checks for is password correct
+	}
+	PortalManager->SignIn(Username, Password);
+}
+
+void USignInOverlay::SignUpButtonClicked()
+{
+	const FString Username = SignUpPage->TextBox_UserName->GetText().ToString();
+	const FString Password = SignUpPage->TextBox_Password->GetText().ToString();
+	const FString Email = SignUpPage->TextBox_Email->GetText().ToString();
+	PortalManager->SignUp(Username, Password, Email);
+}
+
+void USignInOverlay::ConfirmButtonClicked()
+{
+	const FString ConfirmationCode = ConfirmSignUpPage->TextBox_ConfirmationCode->GetText().ToString();
+	ConfirmSignUpPage->Button_Confirm->SetIsEnabled(false);
+	PortalManager->Confirm(ConfirmationCode);
+}
+
+void USignInOverlay::OnSignUpSucceeded()
+{
+	SignUpPage->ClearTextBoxes();
+	ConfirmSignUpPage->TextBlock_Destination->SetText(FText::FromString(PortalManager->LastSignUpResponse.CodeDeliveryDetails.Destination));
+	ShowConfirmSignUpPage();
+}
+
+void USignInOverlay::OnConfirmSucceeded()
+{
+	ConfirmSignUpPage->ClearTextBoxes();
+	ShowSuccessConfirmedPage();
+}
